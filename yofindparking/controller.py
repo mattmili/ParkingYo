e2cc33016ce14037e076eacb0ab24900f9b8cc52
mattmili <matthewmilitante@gmail.com>
2014-11-08 02:31:53 -0500

    fixed errors AGAIN

--- yofindparking/controller.py
+++ yofindparking/controller.py
@@ -22,23 +22,19 @@ def getJSONData(latitude, longitude, username):
     lng=str(longitude)
     url='http://api.parkwhiz.com/search/?lat='+lat+'&lng='+lng+'&key='+config.ParkWhizAPIKey
 
-    try:
-        data = json.load(urllib2.urlopen(url))
-        # Get the closest parking spot
-        closestParkingLot = data.items()[6][1][0]   
-    except Exception:
-        send_yo(username, callbackURL+'/noresult')
-    else:
-        pSpot = parkingSpot(
-            closestParkingLot['city'], 
-            closestParkingLot['lat'], 
-            closestParkingLot['lng'],
-            closestParkingLot['price_formatted'], 
-            closestParkingLot['distance'],
-            closestParkingLot['location_name'],
-            closestParkingLot['available_spots'])
-    
-        return pSpot
+   
+    data = json.load(urllib2.urlopen(url))
+    # Get the closest parking spot
+    closestParkingLot = data.items()[6][1][0]   
+    send_yo(username, callbackURL+'/noresult')
+    pSpot = parkingSpot(closestParkingLot['city'], 
+        closestParkingLot['lat'], 
+        closestParkingLot['lng'],
+        closestParkingLot['price_formatted'], 
+        closestParkingLot['distance'],
+        closestParkingLot['location_name'],
+        closestParkingLot['available_spots'])
+    return pSpot
     
 def send_yo(username, link):
     """Yo a username"""
@@ -83,12 +79,13 @@ def yo():
     spot = getJSONData(latitude, longitude, username)
     #spot = getJSONData(40.748183, -73.985064, 0) # for testing purposes
 
-    parkingLotCity=spot.city
-    parkingLotName=spot.lotName
-    parkingLotDistance=spot.distance
-    parkingLotPrice=spot.price
-
-    link = callbackURL+"/response?name={0}&distance={1}&price={2}&city={3}".format(
-        parkingLotName, parkingLotDistance, parkingLotPrice, parkingLotCity)
-    send_yo(username, link)
+    if spot is None:
+        send_yo(username, callbackURL+"/noresult")
+    else:
+        link = callbackURL+"/response?name={0}&distance={1}&price={2}&city={3}".format(
+            spot.name, 
+            spot.distance, 
+            spot.price, 
+            spot.city)
+        send_yo(username, link)
     return 'OK'
