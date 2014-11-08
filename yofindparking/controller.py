a84bc1a3d0caffb9904afa3c33f666b55947f7ab
mattmili <matthewmilitante@gmail.com>
2014-11-07 20:51:46 -0500

    added .gitignore

--- yofindparking/controller.py
+++ yofindparking/controller.py
@@ -1,12 +1,9 @@
 from yofindparking import app
 from flask import request, render_template
 import requests, urllib2, json, pprint
+import config
 
 YO_API = "https://api.justyo.co/yo/"
-api_token = "e6263ea5-e7ed-4142-bc9c-54c07b41ecfe"
-ParkWhizAPIKey='d9e934eabf61b48b4885179b1c37afd6'
-callbackURL='http://5e96697a.ngrok.com'
-
 
 class parkingSpot:
     def __init__(self, city, lat, lng, cost, distance, lotName, spots):
@@ -21,7 +18,7 @@ class parkingSpot:
 def getJSONData(latitude, longitude, index):
     lat=str(latitude)
     lng=str(longitude)
-    url='http://api.parkwhiz.com/search/?lat='+lat+'&lng='+lng+'&key='+ParkWhizAPIKey
+    url='http://api.parkwhiz.com/search/?lat='+lat+'&lng='+lng+'&key='+config.ParkWhizAPIKey
     data = json.load(urllib2.urlopen(url))   
     
     # Get the closest parking spot
@@ -42,7 +39,7 @@ def send_yo(username, link):
     """Yo a username"""
     requests.post(
         YO_API,
-        data={'api_token': api_token, 'username': username, 'link': link})
+        data={'api_token': config.api_token, 'username': username, 'link': link})
 
 
 @app.route('/')
@@ -86,7 +83,7 @@ def yo():
     parkingLotDistance=spot.distance
     parkingLotPrice=spot.price
         
-    link = callbackURL+"/response?name={0}&distance={1}&price={2}&city={3}".format(
+    link = config.callbackURL+"/response?name={0}&distance={1}&price={2}&city={3}".format(
             parkingLotName, parkingLotDistance, parkingLotPrice, parkingLotCity)
     send_yo(username, link)
     return 'OK'
